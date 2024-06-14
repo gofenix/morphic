cb4faf9a083458511c969af5ec0e12a519939473
gofenix <codeless98@163.com>
2024-06-15 00:04:45 +0800

    use home icon

--- components/header-cosine.tsx
+++ components/header-cosine.tsx
@@ -6,6 +6,8 @@ import HistoryContainer from './history-container'
 import AuthButton from './auth-button'
 import { createClient } from '@/utils/supabase/server'
 import UsedInfo from './used-info'
+import Image from 'next/image'
+import { HomeIcon } from 'lucide-react'
 
 export const Header: React.FC = async () => {
   let count = 0
@@ -14,7 +16,7 @@ export const Header: React.FC = async () => {
     <header className="fixed w-full p-1 md:p-2 flex justify-between items-center z-10 backdrop-blur md:backdrop-blur-none bg-background/80 md:bg-transparent">
       <div className="flex items-center">
         <a href="/">
-          <IconLogo className={cn('w-5 h-5')} />
+          <HomeIcon className={cn('w-5 h-5')} />
           <span className="sr-only">Morphic</span>
         </a>
         <UsedInfo />
